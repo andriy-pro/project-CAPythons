d01c422f9c9be296a98bdf72d80efcb1f5b68a84
DR24DR24 <rhett6butler@gmail.com>
2024-05-24 00:52:30 +0300

    test1 works

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -4,27 +4,68 @@ import sys
 
 import os
 
-import src.presentation
-import src.presentation.cli
-
-#import src.presentation
-#import src.presentation.cli
-
 # Додавання кореневої теки проекту до sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import src
+import src.presentation
+import src.presentation.cli
 
 class Test(unittest.TestCase):
-    def test_function(self):
-        sys.stdin = open("./tests/preprogrammed_inputs.txt")  
+
+    file_names=["test1"]
+    directory="./tests/"
+
+    @unittest.skip("make refs")
+    def tests_CLI_make_ref(self):
+        for name in Test.file_names:
+            self.CLI_make_ref(name)
+
+    def CLI_make_ref(self,name):
+        sys.stdin = open(Test.directory+name+".txt")  
         original_stdout = sys.stdout
-        with open("./tests/otputs_ref.txt", 'w') as f:
+        with open(Test.directory+name+"_ref.txt", 'w') as f:
         # Перенаправляем поток вывода stdout на файл
             sys.stdout = f
-            output=sys.stdout
+            #output=sys.stdout
             src.presentation.cli.main()
             #output=1 #sys.stdout
         sys.stdout = original_stdout    
         #print(type(output))
         #print(dir(output))
-        assert output=="Goodbay"
+        assert True
+
+    #@unittest.skip("make test")
+    def tests_CLI_make_test(self):
+        for name in Test.file_names:
+            self.CLI_make_test(name)
+
+    def CLI_make_test(self,name):
+        sys.stdin = open(Test.directory+name+".txt")  
+        original_stdout = sys.stdout
+        with open(Test.directory+name+"_log.txt", 'w') as f:
+        # Перенаправляем поток вывода stdout на файл
+            sys.stdout = f
+            #output=sys.stdout
+            src.presentation.cli.main()
+            #output=1 #sys.stdout
+        sys.stdout = original_stdout    
+        #print(type(output))
+        #print(dir(output))
+        self.assertTrue(self.compare_text_files(Test.directory+name+"_ref.txt",\
+                                                Test.directory+name+"_log.txt")) 
+
+    def compare_text_files(self,file1_path, file2_path):
+        with open(file1_path, 'r', encoding='utf-8') as file1, \
+            open(file2_path, 'r', encoding='utf-8') as file2:
+        
+        # Считываем содержимое файлов
+            content1 = file1.read()
+            content2 = file2.read()
+
+        # Сравниваем содержимое файлов
+            return content1 == content2
+
+
+if __name__=="__main__":
+    unittest.main()
+ 
