29d95d80f6ba41187dd78c45660d7ac94ec9f9ce
DR24DR24 <rhett6butler@gmail.com>
2024-05-23 23:03:44 +0300

    catch in/out works

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -1,15 +1,30 @@
 #import src.presentation.cli  # The module which contains the call to input
-import src
+import unittest
 import sys
 
-class Test:
-    def test_function(self):
-        sys.stdin = open("./src/tests/preprogrammed_inputs.txt")  # open("preprogrammed_inputs.txt")
-        src.presentation.cli.main()
-        output=sys.stdout
-        with open("firstout.txt","w") as file: 
-            for chunk in output:
-                file.write(chunk)    
+import os
 
-        print(output)
+import src.presentation
+import src.presentation.cli
+
+#import src.presentation
+#import src.presentation.cli
+
+# Додавання кореневої теки проекту до sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import src
+
+class Test(unittest.TestCase):
+    def test_function(self):
+        sys.stdin = open("./tests/preprogrammed_inputs.txt")  
+        original_stdout = sys.stdout
+        with open("./tests/otputs_ref.txt", 'w') as f:
+        # Перенаправляем поток вывода stdout на файл
+            sys.stdout = f
+            output=sys.stdout
+            src.presentation.cli.main()
+            #output=1 #sys.stdout
+        sys.stdout = original_stdout    
+        #print(type(output))
+        #print(dir(output))
         assert output=="Goodbay"
